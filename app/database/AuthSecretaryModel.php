8d874cdb257802523fdee97dced789c864e36fb6
Gustavo Gonzaga <gustavogsantos2013@gmail.com>
2024-05-07 20:06:04 -0300

    feature/logout

--- app/database/AuthSecretaryModel.php
+++ app/database/AuthSecretaryModel.php
@@ -3,10 +3,8 @@ namespace app\database;
 use PDO;
 use Connect;
 use PDOException;
-
 class AuthSecretaryModel extends Connect{
     private $pdo;
-
     public function __construct() {  
         $this->pdo = $this->getConnection();
         
@@ -16,19 +14,21 @@ class AuthSecretaryModel extends Connect{
     }
 
     public function checkUser($usuario, $senha) {
-        $stmt = $this->pdo->prepare("SELECT senha FROM secretaria WHERE usuario = :usuario");
+        $stmt = $this->pdo->prepare("SELECT id, senha FROM secretaria WHERE usuario = :usuario");
         $stmt->bindParam(':usuario', $usuario);
         $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($result) {
+            $id = $result['id'];
+            $hashedPassword = $result['senha'];
 
-        $hashedPassword = $stmt->fetchColumn();
 
-        
-        if ($hashedPassword && password_verify($senha, $hashedPassword)) {
-            return true; // Senha correta
-        } else {
-            return false; // Senha incorreta 
+            if ($hashedPassword && password_verify($senha, $hashedPassword)) {
+                return $id; // Senha correta
+            } else {
+                return null; // Senha incorreta 
+            }
         }
     }
 }
-
 ?>
