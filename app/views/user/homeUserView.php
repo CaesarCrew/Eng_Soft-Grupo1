c6efbd3bd3703ce755090489063b4f4030f6de22
Gustavo Gonzaga dos Santos <gustavo.gonzaga@to.je>
2024-06-14 16:35:20 -0300

    Atualizando as pastas do projeto

--- app/views/user/homeUserView.php
+++ app/views/user/homeUserView.php
@@ -1,16 +1,16 @@
-
-
 <body>
-
-    <div class="container">
-        <button class="button" onclick="window.location.href='http://localhost/agendarHorariosPaciente'">Agendar Consulta</button>
-        <button class="button" onclick="window.location.href='http://localhost/visualizarAgendamentosUsuario'">Visualizar Agendamentos</button>
+    <div class="header">
+        <a href="#">HealthConnect</a>
     </div>
-
+    <div class="container">
+        <h1 class="title">Painel Usuário</h1>
+        <form>
+            <button type="button" onclick="location.href='http://localhost/agendarHorariosPaciente'" class="login-button">Agendar Consulta</button>
+            <button type="button" onclick="location.href='http://localhost/visualizarAgendamentosUsuario'" class="login-button">Visualizar Agendamentos</button>
+        </form>
+    </div>
     <form method="POST" action="/logout" class="logout">
         <button type="submit">Logout</button>
     </form>
-
 </body>
-
 </html>
